6a2548a32f6178eaebccfea1f810c591a65d7797
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-04-10 23:14:17 -0300

    Adding dynamic data to questions and answers

--- flutter/first_project_course/lib/main.dart
+++ flutter/first_project_course/lib/main.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './questions.dart';
+import './answer.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex < 2) {
+        _questionIndex = _questionIndex + 1;
+      }
+    });
+    print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ["Black", "Blue", "White", "Red"]
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ["Dog", "Cat", "Fish", "Bird"]
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': ["Maxmillian", "Akita", "John", "Will"]
+      }
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) => (Answer(_answerQuestion, answer)))
+                .toList(),
+          ],
+        ),
+      ),
     );
   }
 }
