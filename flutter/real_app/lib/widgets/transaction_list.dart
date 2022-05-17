af6bab6ea3f7c5c763494899a3356d35e48a7374
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-05-17 16:34:27 -0300

    dynamic constraints, mediaquery, responsive

--- flutter/real_app/lib/widgets/transaction_list.dart
+++ flutter/real_app/lib/widgets/transaction_list.dart
@@ -11,59 +11,59 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 450,
-        child: (transactions.isEmpty
-            ? Column(
-                children: [
-                  Text(
-                    'No transactions added yet!',
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (context, constraints) {
+            return Column(
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text(
+                        '\$${transactions[index].amount}',
+                      )),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
                     style: Theme.of(context).textTheme.headline6,
                   ),
-                  SizedBox(
-                    height: 20,
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                    onPressed: () => deleteTx(transactions[index].id),
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
                   ),
-                  Container(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(
-                              child: Text(
-                            '\$${transactions[index].amount}',
-                          )),
-                        ),
-                      ),
-                      title: Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      subtitle: Text(
-                          DateFormat.yMMMd().format(transactions[index].date)),
-                      trailing: IconButton(
-                        onPressed: () => deleteTx(transactions[index].id),
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                      ),
-                    ),
-                  );
-                },
-                itemCount: transactions.length,
-              )));
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
